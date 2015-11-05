7738225e5e378e84465b084516f7e12383138306
Maira Lima <maijucelima@gmail.com>
2015-11-05 00:04:16 -0200

    criacao de interfaes

--- src/main/webapp/js/cdweb/routes.js
+++ src/main/webapp/js/cdweb/routes.js
@@ -5,20 +5,34 @@ angular.module("cdweb")
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider
             .when('/wellcome', {
-                templateUrl: 'wellcome.html'
+                templateUrl: 'wellcome.html',
+                controller:'CadController'
             })
             .when('/login', {
                 templateUrl: 'login.html'
             })
 
+
             .when('/config', {
                 templateUrl: 'configuration.html',
                 controller: 'ConfigurationController'
             })
+
+
             .when('/menu', {
                 templateUrl: 'menu.html',
                 controller: 'MenuController'
             })
+
+            .when('/predefinicoes', {
+                templateUrl: 'predefinicoes.html'
+            })
+
+            .when('/dispositivo', {
+                templateUrl: 'dispositivo.html'
+            })
+
+
             .when('/Book/:bookId/ch/:chapterId', {
                 templateUrl: 'chapter.html',
                 controller: 'ChapterController'
